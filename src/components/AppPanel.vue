9c9ad2c707a28cc86b48062ca8fc7c754e45ac37
qzh1991 <qzh1991@qq.com>
2017-12-25 12:22:11 +0800

    .

--- src/components/AppPanel.vue
+++ src/components/AppPanel.vue
@@ -1,16 +1,14 @@
 <template>
 <div>
   <search
-    :results="results"
     position="absolute"
     auto-scroll-to-top 
-    @on-result-click="resultClick"
-    @on-change="getResult"
+    @on-change="onSearchChange"
     @on-focus="onFocus"
     @on-cancel="onCancel"
     @on-submit="onSubmit"
     ref="search">
-      <router-link slot="right" :to="link">新增</router-link>
+      <router-link slot="right" :to="link" class="weui-search-bar__add-btn">新增</router-link>
     </search>
   <div v-if="title" class="weui-cells__title">{{title}}</div>
   <div class="weui-panel weui-panel_access">    
@@ -55,18 +53,16 @@
         }
         return clink
       },
-      resultClick (item) {
-        window.alert('you click the result item: ' + JSON.stringify(item))
-      },
-      getResult (val) {
-
-        this.results = val ? getResult(this.value) : []
+      onSearchChange (val) {
+        this.filterList = this.list.filter(v => {
+          return v.Name.indexOf(val) > -1
+        })
       },
       onFocus () {
         console.log('on focus')
       },
       onCancel () {
-        console.log('on cancel')
+        this.filterList=this.list
       },
       onSubmit () {
         this.$refs.search.setBlur()
@@ -80,3 +76,11 @@
     }
   }
 </script>
+<style scoped>
+.weui-search-bar__add-btn{
+  margin-left: 10px;
+  line-height: 28px;
+  color: #09BB07;
+  white-space: nowrap;
+}
+</style>
--- src/page/1KangFuRuHu/KangFuRuHuHome.vue
+++ src/page/1KangFuRuHu/KangFuRuHuHome.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <!-- <x-header :left-options="{showBack:false}" style="width:100%;position:absolute;left:0;top:0;z-index:100;">
-      精准康复入户
-      <router-link slot="right" to="/KangFuRuHuDetail"><i class="fa fa-plus fa-lg"></i></router-link>
-    </x-header> -->
-    <app-panel title="精准康复入户患者列表" :list="list" :link="link" type="info"></app-panel>
+    <app-panel :title="title" :list="list" :link="link" type="info"></app-panel>
   </div>
 </template>
 <script>
@@ -16,6 +12,7 @@ export default {
   },
   data () {
     return {
+      title:"精准康复入户患者列表",
       link:"KangFuRuHuDetail",
       list: [],
     }
@@ -27,17 +24,6 @@ export default {
     initData () {
       this.$api.GetDisabledInfoes().then(res => { this.list = res })
     },
-    
   }
-}
-function getResult (val) {
-  let rs = []
-  for (let i = 0; i < 20; i++) {
-    rs.push({
-      title: `${val} result: ${i + 1} `,
-      other: i
-    })
-  }
-  return rs
 }
 </script>
