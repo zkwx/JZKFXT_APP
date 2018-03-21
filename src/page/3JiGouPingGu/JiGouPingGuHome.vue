7db13bf14df04d0e4cb38c057c29608d6a552183
727841493 <727841493@qq.com>
2018-03-21 14:34:32 +0800

    .

--- src/page/3JiGouPingGu/JiGouPingGuHome.vue
+++ src/page/3JiGouPingGu/JiGouPingGuHome.vue
@@ -8,56 +8,48 @@
   </div>
 </template>
 <script>
-import {Group,Cell} from 'vux'
+import { Group, Cell } from "vux";
 export default {
-    components: {
-      Group,
-      Cell,
+  components: {
+    Group,
+    Cell
   },
-  data () {
+  data() {
     return {
-      title:"机构评估与审核",
-      menus:[
+      title: "机构评估与审核",
+      menus: [
         {
-          title:'辅具审核',
-          isLink:true,
-          link:'/ExamList/type/FuJuShenHe/categoryTabs',
-          img:require('../../assets/icon/i1.png')
+          title: "辅具审核",
+          isLink: true,
+          link: "/ExamList/type/FuJuShenHe/categoryTabs",
+          img: require("../../assets/icon/i1.png")
         },
         {
-          title:'假肢矫形器审核',
-          isLink:true,
-          link:'/ExamList/type/JiaZhiJiaoXingQiShenHe/jiaZhiJiaoXingQiTabs',
-          img:require('../../assets/icon/i2.png')
+          title: "假肢矫形器审核",
+          isLink: true,
+          link: "/ExamList/type/JiaZhiJiaoXingQiShenHe/jiaZhiJiaoXingQiTabs",
+          img: require("../../assets/icon/i2.png")
         },
         {
-          title:'无障碍审核',
-          isLink:true,
-          link:'/ExamList/name/WuZhangAiShenHe/wuZhangAiTabs',
-          img:require('../../assets/icon/i3.png')
+          title: "无障碍审核",
+          isLink: true,
+          link: "/ExamList/name/WuZhangAiShenHe/wuZhangAiTabs",
+          img: require("../../assets/icon/i3.png")
         },
         {
-          title:'机构评估',
-          isLink:true,
-          link:'/ExamList/type/JiGouPingGu/categoryTabs',
-          img:require('../../assets/icon/i3.png')
-        },
-      ],
-    }
+          title: "机构评估",
+          isLink: true,
+          link: "/ExamList/type/JiGouPingGu/categoryTabs",
+          img: require("../../assets/icon/i3.png")
+        }
+      ]
+    };
   },
-  created () {
-    
-  },
-  methods: {
-    
-  },
-  computed:{
-    
-  },
-  watch:{
-   
-  }
-}
+  created() {},
+  methods: {},
+  computed: {},
+  watch: {}
+};
 </script>
 
 <style scoped>
